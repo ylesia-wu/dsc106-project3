cb942899d7719330f7c7e795828655788fa56321
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-02-16 14:55:53 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,76 +4,96 @@
     import * as d3 from 'd3';
 
     // <!-- load csv data -->
-    let data = [];
+    // let data = [];
+    let state = [];
+    let numDrivers = [];
+    let speeding = [];
+    let alcohol = [];
+    let notDistracted = [];
+    let noPrevious = [];
 
     onMount(async () => {
         const res = await fetch('bad-drivers.csv');
         const csv = await res.text();
 
         await d3.csvParse(csv, (d, i, columns) => {
-            for (let i = 1; i < 2; ++i) {
-                data.push({
-                    state: d['State'],
-                    numDrivers: d['Number of drivers involved in fatal collisions per billion miles'],
-                    speeding: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Speeding'],
-                    alcohol: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Alcohol-Impaired'],
-                    notDistracted: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Not Distracted'],
-                    noPrevious: d['Percentage Of Drivers Invovled In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents']
-                })
-            }
+            // for (let i = 1; i < 2; ++i) {
+                // state.push({
+                //     state: d['State'],
+                //     numDrivers: d['Number of drivers involved in fatal collisions per billion miles'],
+                //     speeding: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Speeding'],
+                //     alcohol: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Alcohol-Impaired'],
+                //     notDistracted: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Not Distracted'],
+                //     noPrevious: d['Percentage Of Drivers Invovled In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents']
+                // })
+                state.push(d['State']);
+                numDrivers.push({state: d['State'], value: d['Number of drivers involved in fatal collisions per billion miles']});
+                speeding.push({state: d['State'], value: d['Percentage Of Drivers Involved In Fatal Collisions Who Were Speeding']})
+                alcohol.push({state: d['State'], value: d['Percentage Of Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired']});
+                notDistracted.push({state: d['State'], value: d['Percentage Of Drivers Involved In Fatal Collisions Who Were Not Distracted']});
+                noPrevious.push({state: d['State'], value: d['Percentage Of Drivers Involved In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents']})
+            // }
         });
-        data = data;
+        // data = data;
+        // state = state;
+        // numDrivers = numDrivers;
+        // speeding = speeding;
+        // alcohol = alcohol;
+        // notDistracted = notDistracted;
+        // noPrevious = noPrevious;
     });
+
+    // console.log(noPrevious);
 
     let xScale;
     let yScale;
 
     // set the dimensions and margins of the graph
-    // const margin = {top: 30, right: 30, bottom: 30, left: 30};
-    // const width = 400;
-    // const height = 400;
+    const margin = {top: 20, right: 20, bottom: 20, left: 20};
+    const width = 840;
+    const height = 600;
 
     // let currentData = data.map(d => ({state: d.state, value: d.numDrivers}));
 
     // Set up scales on component mount
     // onMount(() => {
-    //     // Initialize scales
-    //     xScale = scaleBand()
-    //     .domain(currentData.map(d => d.state))
-    //     .range([0, 400]) // Adjust the range as needed
-    //     .padding(0.1);
+        // Initialize scales
+    $: xScale = d3.scaleBand()
+                .domain(state.map((d) => d))
+                .range([0, width]) // Adjust the range as needed
+                .padding(0.1);
 
-    //     yScale = scaleLinear()
-    //     .domain([0, d3.max(currentData, d => d.value)])
-    //     .range([400, 0]); // Adjust the range as needed
+    $: yScale = d3.scaleLinear()
+                .domain([0, d3.max(numDrivers, (d) => d.value)])
+                .range([height, 0]); // Adjust the range as needed
     // });
 
-    function update(selectedVar) {
+    // function update(selectedVar) {
         
-        currentVar = selectedVar;
+    //     currentVar = selectedVar;
 
-        // currentData = data.map(d => ({state: d.state, value: d.selectedVar}));
+    //     // currentData = data.map(d => ({state: d.state, value: d.selectedVar}));
 
-        // yScale = scaleLinear()
-        //         .domain([0, d3.max(currentData, d => d.value)])
-        //         .range([400, 0]);
+    //     // yScale = scaleLinear()
+    //     //         .domain([0, d3.max(currentData, d => d.value)])
+    //     //         .range([400, 0]);
 
-    }
+    // }
 
 </script>
 
 <main>
 
     <!-- Add 5 buttons -->
-    <button on:click={() => update('numDrivers')}>Total Number</button>
+    <!-- <button on:click={() => update('numDrivers')}>Total Number</button>
     <button on:click={() => update('speeding')}>Speeding</button>
     <button on:click={() => update('alcohol')}>Alcohol</button>
     <button on:click={() => update('notDistrcted')}>Not Distracted</button>
-    <button on:click={() => update('noPrevious')}>No Previous Accident</button>
+    <button on:click={() => update('noPrevious')}>No Previous Accident</button> -->
 
-    <svg width=400 height=400>
+    <svg {width} {height}>
         <!-- x axis -->
-		<g class="axis x-axis">
+		<!-- <g class="axis x-axis">
 			{#each currentData as d}
 				<text
                     x={xScale(d.state) + xScale.bandwidth() / 2}
@@ -84,10 +104,10 @@
                     {d.state}
                 </text>
 			{/each}
-		</g>
+		</g>  -->
 
-        y axis
-		<g class="axis y-axis">
+        <!-- y axis -->
+		<!-- <g class="axis y-axis">
 			<line x1="0" y1="400" x2="0" y2="0" stroke="black" />
             {#each yScale.ticks() as tick}
                 <g transform="translate(0, {yScale(tick)})">
@@ -95,19 +115,21 @@
                     <text x="-10" y="0" text-anchor="end" alignment-baseline="middle">{tick}</text>
                 </g>
             {/each}
-		</g>
+		</g> -->
 
-        <!-- bars
-        <g class="bars">
-			{#each currentData as state, value}
-				<rect
-					x={xScale(state)}
-					y={yScale(value)}
-					width={8}
-					height={yScale(0) - yScale(value)}
+        
+
+        <!-- bars -->
+        <!-- <g class="bars"> -->
+			{#each numDrivers as data, i}
+				<rect	
+                    x={xScale(data.state)}
+                    y={0}
+                    width={xScale.bandwidth()}
+                    height={yScale(data.value)}
 				/>
 			{/each}
-		</g> -->
+		<!-- </g> -->
     </svg>
 
     
@@ -140,8 +162,8 @@
 </main>
 
 <style>
-    main {
-         
+    rect {
+       fill: blue;  
     }
 </style>
 
