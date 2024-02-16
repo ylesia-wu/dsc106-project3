d4d79ffbc70b2136aff09e5cd5cc98f790461126
Rongjing Jiang <gracejiang0929@gmailcom>
2024-02-15 16:04:44 -0800

    load data

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,3 +1,56 @@
 <script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+    // import Temperature from './Temperature.svelte';
+    // import Temperature01Marks from './Temperature01Marks.svelte';
+    // import Temperature02Channels from './Temperature02Channels.svelte';
+    // import Temperature03Axes from './Temperature03Axes.svelte';
+    // import Temperature04Interactive from './Temperature04Interactive.svelte';
+
+    // <!-- load csv data -->
+    let data = [];
+
+    onMount(async () => {
+        const res = await fetch(
+        'bad-drivers.csv',
+        );
+        const csv = await res.text();
+        console.log(csv);
+
+        // await d3.csvParse(csv, (d, i, columns) => {
+        // // for (let i = 1; i < 13; ++i) {
+        //     // console.log(d[i]);
+
+        // //     // pivot longer
+        // //     // data.push({
+            
+        // //     // date: new Date(Date.UTC(d.Year, i - 1, 1)),
+        // //     // value: +d[columns[i]],
+        // //     // });
+        // // }
+        // });
+        // data = data;
+    });
+
+
+
+
+
+
+
+
+
+
 
 </script>
+
+<main>
+    <!-- Add HTML elements here -->
+</main>
+
+<style>
+    main {
+        
+    }
+</style>
+
