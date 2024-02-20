18b407fc9032a10bde11c118a8237b6aa38a4b30
Rongjing Jiang <gracejiang0929@gmailcom>
2024-02-19 20:36:42 -0800

    add title

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -18,14 +18,6 @@
 
         await d3.csvParse(csv, (d, i, columns) => {
 
-                // state.push({
-                //     state: d['State'],
-                //     numDrivers: d['Number of drivers involved in fatal collisions per billion miles'],
-                //     speeding: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Speeding'],
-                //     alcohol: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Alcohol-Impaired'],
-                //     notDistracted: d['Percentage Of Drivers Invovled In Fatal Collisions Who Were Not Distracted'],
-                //     noPrevious: d['Percentage Of Drivers Invovled In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents']
-                // })
                 state.push({state: d['State'], value: 0});
                 numDrivers.push({state: d['State'], value: d['Number of drivers involved in fatal collisions per billion miles']});
                 speeding.push({state: d['State'], value: d['Percentage Of Drivers Involved In Fatal Collisions Who Were Speeding']})
@@ -53,36 +45,35 @@
     const height = 680;
 
     // Set up scales on component mount
-    // onMount(() => {
-        // Initialize scales
+   
     $: xScale = d3.scaleBand()
                 .domain(currentData.map(d => d.state))
                 .range([margin.left, width - margin.right]) // Adjust the range as needed
                 .padding(0.1);
 
-    // $: yScale = d3.scaleLinear()
-    //             // .domain([0, d3.max(currentData, d => d.value)])
-    //             .domain([0, Math.min(25, d3.max(currentData, d => d.value))])
-    //             // .domain([0, 25])
-
-    //             .range([height - margin.bottom, margin.top]); // Adjust the range as needed
-    // });
-
     let y_label = '';
+    let subtitle = '';
 
     function update(selectedData) {
         currentData = selectedData;
 
         if (selectedData === numDrivers) {
-            y_label = 'Number of drivers involved in fatal collisions per billion miles';
+            y_label = 'Number of Drivers Involved in Fatal Collisions Per Billion Miles';
+            subtitle = 'Number of Drivers Involved in Fatal Collisions Per Billion Miles in 50 US States';
         } else if (selectedData === speeding) {
-            y_label = 'Drivers Involved In Fatal Collisions Who Were Speeding (%)'
+            y_label = 'Drivers Involved In Fatal Collisions Who Were Speeding (%)';
+            subtitle = 'Drivers Involved In Fatal Collisions Who Were Speeding (%) in 50 US States';
         } else if (selectedData === alcohol) {
-            y_label = 'Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired (%)'
+            y_label = 'Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired (%)';
+            subtitle = 'Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired (%) in 50 US States';
+
         } else if (selectedData === notDistracted) {
-            y_label = 'Drivers Involved In Fatal Collisions Who Were Not Distracte (%)'
+            y_label = 'Drivers Involved In Fatal Collisions Who Were Not Distracte (%)';
+            subtitle = 'Drivers Involved In Fatal Collisions Who Were Not Distracte (%) in 50 US States';
         } else if (selectedData === noPrevious) {
-            y_label = 'Drivers Involved In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents (%)'
+            y_label = 'Drivers Involved In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents (%)';
+            subtitle = 'Drivers Involved In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents (%) in 50 US States';
+
         } else {
             y_label = 'Default Y-Axis Label';
         }
@@ -94,10 +85,7 @@
 
     $: {
         yScale = d3.scaleLinear()
-                // .domain([0, d3.max(currentData, d => d.value)])
             .domain([0, Math.max(25, d3.max(currentData, d => d.value))])
-                // .domain([0, 25])
-
             .range([height - margin.bottom, margin.top]); // Adjust the range as needed
         
         d3.select(yAxis).call(d3.axisLeft(yScale));
@@ -118,42 +106,13 @@
 
     <!-- Add 5 buttons -->
     <button on:click={() => update(numDrivers)}>Total Number</button>
-    <!-- <button on:click={() => function (d) {currentData = numDrivers}}>Total Number</button> -->
     <button on:click={() => update(speeding)}>Speeding</button>
     <button on:click={() => update(alcohol)}>Alcohol</button>
     <button on:click={() => update(notDistracted)}>Not Distracted</button>
     <button on:click={() => update(noPrevious)}>No Previous Accident</button>
-    <!-- <button on:click={() => update(numDrivers)}>Total Number</button> -->
 
     <svg {width} {height}>
     
-
-        <!-- x axis -->
-		<!-- <g class="axis x-axis">
-			{#each currentData as data}
-				<text
-                    x={xScale(data.state)}
-                    y={height}
-                    text-anchor="middle"
-                    rotate="-90"
-                    font-size="10"
-                >
-                    {data.state}
-                </text>
-			{/each}
-		</g>  -->
-
-        <!-- y axis -->
-		<!-- <g class="axis y-axis">
-			<line x1="0" y1="400" x2="0" y2="0" stroke="black" />
-            {#each yScale.ticks() as tick}
-                <g transform="translate(0, {yScale(tick)})">
-                    <line x1="-5" y1="0" x2="0" y2="0" stroke="black" />
-                    <text x="-10" y="0" text-anchor="end" alignment-baseline="middle">{tick}</text>
-                </g>
-            {/each}
-		</g> -->
-        <!-- {console.log('Inside for loop:', numDrivers)} -->
         
         <g class="x-axis">
             <text transform={`translate(${width / 2}, ${height - 10})`} text-anchor="middle">US States</text>
@@ -162,10 +121,10 @@
 
         <g class="y-axis">
             <text transform={`translate(${margin.left / 2}, ${height / 2}) rotate(-90)`} text-anchor="middle">{y_label}</text>
-            <!-- You ca
+            
 
         <!-- bars -->
-        <!-- <g class="bars"> -->
+        <g class="bars">
 			{#each currentData as data}
 				<rect	
                     x={xScale(data.state)}
@@ -174,7 +133,7 @@
                     height={height - margin.bottom - yScale(data.value)}
 				/>
 			{/each}
-		<!-- </g> -->
+		</g>
 
         <g transform="translate(0, {height - margin.bottom})"
         bind:this={xAxis} />
@@ -182,40 +141,19 @@
         <g transform="translate({margin.left}, 0)"
         bind:this={yAxis} />
 
+        <!-- Title -->
+        <text x="50%" y="30" font-size="24" text-anchor="middle">Good Drivers? Bad Drivers?</text>
+    
+        <!-- Subtitle -->
+        <text x="50%" y="60" font-size="16" text-anchor="middle">{subtitle}</text>
+
     </svg>
 
-    
-    <!-- <button
-        aria-label="Total Number"
-        on:click={() => update(numDrivers)}
-        class="update"
-    />
-    <button
-        aria-label="Speeding"
-        on:click={() => update(speeding)}
-        class="update"
-    />
-    <button
-        aria-label="Alcohol"
-        on:click={() => update(alcohol)}
-        class="update"
-    />
-    <button
-        aria-label="NotDistracted"
-        on:click={() => update(notDistracted)}
-        class="update"
-    />
-    <button
-            aria-label="NoPrevious"
-            on:click={() => update(noPrevious)}
-            class="update"
-    /> -->
+
 
 </main>
 
-<!-- <style>
-    rect {
-       fill: blue;  
-    }
-</style> -->
+<style>
+    
+</style>
 
